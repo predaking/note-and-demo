cfd48755f6d877f5103d4c192194a334a3fcc1fa
predaking <413255798@qq.com>
2021-11-03 17:59:58 +0800

    fix: none

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {
+    useState,
+    useEffect,
+} from 'react';
+
+import {
+    Button
+} from 'antd';
+
+import './index.css';
+
+// import DemoComponent from './DemoComponent/index.jsx';
 
 export default function App() {
+
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        console.log('effect1');
+    }, [count]);
+
+    useEffect(() => {
+        console.log('effect2');
+    }, [count]);
+
+    useEffect(() => {
+        console.log('effect3');
+    }, [count]);
+
     return (
-		<ul>
-			{
-			  	new Array(3000).fill('').map((item, index) => <li key={index}>{index}</li>)
-		    }
-		</ul>
+        <div className="container">
+            <div className="div-1">按钮</div>
+            <div className="div-2">div2</div>
+            <div className="div-3">div3</div>
+        </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-// import Shelf from './Shelf/index.jsx';
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('app')
-// );
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
 
-ReactDOM.unstable_createRoot(document.getElementById('app')).render(<App />);
+// ReactDOM.unstable_createRoot(document.getElementById('app')).render(<App />);
