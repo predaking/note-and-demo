63b68a3a5e3ef8974f67377b6352cd3b1b8fa5b4
gongwei02 <gongwei02@xdfzx.com>
2024-02-02 18:08:47 +0800

    feat: rust-*

--- rust/demo/src/main.rs
+++ rust/demo/src/main.rs
@@ -1,35 +1,83 @@
-use std::{vec, cmp};
-
 struct Solution {}
 
 impl Solution {
-    pub fn insert(mut intervals: Vec<Vec<i32>>, mut new_interval: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut stack = Vec::new();
-        loop {
-            if intervals.is_empty() {
-                stack.append(&mut vec![new_interval]);
-                return stack;
+    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        let mut res = vec![vec![0; n as usize]; n as usize];
+
+        let (mut i, mut j) = (0, 0);
+        let mut direction = 0;
+
+        const LTR: i32 = 0;
+        const TTB: i32 = 1;
+        const RTL: i32 = 2;
+        const BTT: i32 = 3;
+
+        let mut count = 1;
+        let total = n * n;
+
+        while count <= total {
+            res[i][j] = count;
+    
+            match direction {
+                LTR =>
+                    if j as i32 == n - 1 {
+                        i += 1;
+                        direction = TTB;
+                    } else {
+                        if res[i][j + 1] > 0 {
+                            direction = TTB;
+                            i += 1;
+                        } else {
+                            j += 1;
+                        }
+                    }
+                TTB =>
+                    if i as i32 == n - 1 {
+                        j -= 1;
+                        direction = RTL;
+                    } else {
+                        if res[i + 1][j] > 0 {
+                            direction = RTL;
+                            j -= 1;
+                        } else {
+                            i += 1;
+                        }
+                    }
+                RTL =>
+                    if j == 0 {
+                        i -= 1;
+                        direction = BTT;
+                    } else {
+                        if res[i][j - 1] > 0 {
+                            direction = BTT;
+                            i -= 1;
+                        } else {
+                            j -= 1;
+                        }
+                    }
+                BTT =>
+                    if i == 0 {
+                        j += 1;
+                        direction = LTR;
+                    } else {
+                        if res[i - 1][j] > 0 {
+                            direction = LTR;
+                            j += 1;
+                        } else {
+                            i -= 1;
+                        }
+                    }
+                _ => {}
             }
 
-            if new_interval[1] < intervals[0][0] {
-                intervals.insert(0, new_interval);
-                stack.append(&mut intervals);
-                return stack;
-            }
-
-            if new_interval[0] > intervals[0][1] {
-                stack.append(&mut vec![intervals.remove(0)]);
-                continue;
-            }
-
-            let tmp = vec![cmp::min(new_interval[0], intervals[0][0]), cmp::max(new_interval[1], intervals[0][1])];
-            new_interval = tmp;
-            intervals.remove(0);
+            count += 1;
         }
+
+        res
     }
 }
 
 fn main() {
-    let res = Solution::insert(vec![vec![1, 2], vec![3, 5], vec![6, 7], vec![8, 10], vec![12, 16]], vec![4, 8]);
-    println!("debug-res: {:?}", res);
+    let res = Solution::generate_matrix(5);
+    println!("{:?}", res);
 }
