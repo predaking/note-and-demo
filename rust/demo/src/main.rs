06b3d3efb2bb199d03ee9e308e2337b57ef17ff4
gongwei02 <gongwei02@xdfzx.com>
2024-02-04 18:33:25 +0800

    feat: rust-*

--- rust/demo/src/main.rs
+++ rust/demo/src/main.rs
@@ -1,83 +1,35 @@
 struct Solution {}
 
 impl Solution {
-    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
-        let mut res = vec![vec![0; n as usize]; n as usize];
-
-        let (mut i, mut j) = (0, 0);
-        let mut direction = 0;
-
-        const LTR: i32 = 0;
-        const TTB: i32 = 1;
-        const RTL: i32 = 2;
-        const BTT: i32 = 3;
-
-        let mut count = 1;
-        let total = n * n;
-
-        while count <= total {
-            res[i][j] = count;
-    
-            match direction {
-                LTR =>
-                    if j as i32 == n - 1 {
-                        i += 1;
-                        direction = TTB;
-                    } else {
-                        if res[i][j + 1] > 0 {
-                            direction = TTB;
-                            i += 1;
-                        } else {
-                            j += 1;
-                        }
-                    }
-                TTB =>
-                    if i as i32 == n - 1 {
-                        j -= 1;
-                        direction = RTL;
-                    } else {
-                        if res[i + 1][j] > 0 {
-                            direction = RTL;
-                            j -= 1;
-                        } else {
-                            i += 1;
-                        }
-                    }
-                RTL =>
-                    if j == 0 {
-                        i -= 1;
-                        direction = BTT;
-                    } else {
-                        if res[i][j - 1] > 0 {
-                            direction = BTT;
-                            i -= 1;
-                        } else {
-                            j -= 1;
-                        }
-                    }
-                BTT =>
-                    if i == 0 {
-                        j += 1;
-                        direction = LTR;
-                    } else {
-                        if res[i - 1][j] > 0 {
-                            direction = LTR;
-                            j += 1;
-                        } else {
-                            i -= 1;
-                        }
-                    }
-                _ => {}
-            }
-
-            count += 1;
+    pub fn get_permutation(mut n: i32, mut k: i32) -> String {
+        if n == 1 {
+            return "1".to_string();
         }
 
-        res
+        let mut ans: Vec<i32> = Vec::new();
+        let mut arr: Vec<i32> = (1..=n).collect();
+
+        let factarial = |n: usize| -> i32 {
+            return (1..=n).fold(1, |acc, cur| acc * cur as i32);
+        };
+
+        while n > 0 {
+            let tmp = factarial(n as usize - 1);
+            let idx = (k + tmp - 1) / tmp - 1;
+            if idx >= 0 {
+                ans.push(arr.remove(idx as usize));
+            } else {
+                ans.push(arr.pop().unwrap_or(0));
+            }
+            k %= tmp;
+            n -= 1;
+        }
+
+        ans.iter().map(|vl| vl.to_string()).collect::<Vec<String>>().join("")
     }
 }
 
 fn main() {
-    let res = Solution::generate_matrix(5);
-    println!("{:?}", res);
+    let res = Solution::get_permutation(4, 3);
+    println!("{}", res);
 }
